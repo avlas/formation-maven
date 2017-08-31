2bf0c1b5f6f5872d037bafa6a76ec6bab9c2a7ef
Avl <alina.vlas@gmail.com>
2017-08-31 14:42:48 +0200

    implement a Maven plugin

---
+++ maven_tp5/src/main/java/fr/diginamic/Addition.java
@@ -0,0 +1,13 @@
+package fr.diginamic;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		Integer a = Integer.valueOf(args[0]);
+		Integer b = Integer.valueOf(args[1]);
+		
+		System.out.println("a+b="+(a+b));
+
+	}
+
+}
